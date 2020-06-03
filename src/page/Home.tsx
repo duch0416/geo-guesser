693bf3260392309bb4d2e04ceb83b34ece6a5e6e
duch0416 <mikolajduchewicz@gmail.com>
2020-06-03 16:13:54 +0200

    configure react street view

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,21 +3,17 @@ import styled from "styled-components";
 
 import { useRandomCordinates } from "../hooks/useRandomCordinates";
 import Map from "../map/MapContainer";
+import StreetView from "../streetView/StreetView"
 
-const Wrapper = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: red;
-`;
+
 
 const Home: React.SFC = () => {
   const { getRandomCordinates } = useRandomCordinates();
 
   return (
-    // <Wrapper onClick={getRandomCordinates}></Wrapper>
     <>
       <h1>My google map</h1>
-      <Map></Map>
+      <StreetView></StreetView>
     </>
   );
 };
